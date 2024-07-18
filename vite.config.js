2cd7d671c96b3ca80c8a4d1e07385220a612dff9
Rohan Rajput <82023967+rohan-codez@users.noreply.github.com>
2024-07-18 21:24:28 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+   build: {
+    rollupOptions: {
+      external: ['/src/main.jsx'] // Add the module you want to externalize here
+    }
+  }
 })
